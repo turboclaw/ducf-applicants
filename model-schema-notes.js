2713d72fbcf4ba582807c128bc3fc407e6b96884
turboclaw <dan@turboclaw.com>
2014-03-31 13:41:51 -0400

    notes

---
+++ model-schema-notes.js
@@ -0,0 +1,26 @@
+var admins = [
+	{
+		_id: 1,
+		name: firstname lastname
+	}
+],
+
+var applicants = [
+	{
+		_id: 1,
+		name: "",
+		phone: "",
+		votes: [
+			{
+				adminId: 1,
+				score: 10
+			}
+		],
+		comments: [
+			{
+				adminId: 1,
+				comment: "Pellentesque habitant morbi tristique senectus..."
+			}
+		]
+	}
+]
